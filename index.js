a70f17b7ef2a640d24f0851228fde10966b46e7a
abdul rafay <abdulrafaycodeder@email.com>
2026-01-06 13:13:43 -0800

    assigment start

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function showpara() {
-    var fullpara = '<h1>Experience projects involving web development, data analysis, and machine learning. My experience includes internships at tech companies where I contributed to developing user-friendly applications and optimizing algorithms for better performance. I am proficient in various programming languages such as JavaScript, Python, and Java, and have a strong understanding of software development principles.</h1>';
-    var smallpara = document.getElementById("expent");
-    smallpara.innerText = fullpara;
-    console.log("fullpara shown", fullpara);
-    console.log("smallpara shown", smallpara);
-}
 
 
 var isExpanded = false;
