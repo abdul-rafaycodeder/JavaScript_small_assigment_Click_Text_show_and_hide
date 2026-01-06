7b4d0d9c53c8f2d75c8b12697aa6ef0b5dd9b092
abdul rafay <abdulrafaycodeder@email.com>
2026-01-06 13:09:18 -0800

    JavaScript_small_assigment_Click_Text show and hide

---
+++ index.js
@@ -0,0 +1,19 @@
+function showpara() {
+    var fullpara = '<h1>Experience projects involving web development, data analysis, and machine learning. My experience includes internships at tech companies where I contributed to developing user-friendly applications and optimizing algorithms for better performance. I am proficient in various programming languages such as JavaScript, Python, and Java, and have a strong understanding of software development principles.</h1>';
+    var smallpara = document.getElementById("expent");
+    smallpara.innerText = fullpara;
+    console.log("fullpara shown", fullpara);
+    console.log("smallpara shown", smallpara);
+}
+
+
+var isExpanded = false;
+
+function paragraph() {
+    var shortpara = '  Slow lorises are a group of several species of strepsirrhine primates which make up the genus Nycticebus'
+
+    var fullpara = 'Text chan'
+
+}
+
+
